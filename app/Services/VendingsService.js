59c94d93ab70ffba248b83537fcd6b65d19d8868
cjgreenie <seejarom@gmail.com>
2021-07-13 14:41:57 -0600

    getting going

--- app/Services/VendingsService.js
+++ app/Services/VendingsService.js
@@ -2,11 +2,13 @@ import { ProxyState } from "../AppState.js";
 
 
 class VendingsService {
-  addValue() {
-    ProxyState.values = [...ProxyState.values, new Value({ title: Math.random() })]
-  }
+
   buySnack() {
+
+
+
     console.log('buy snack from the services')
+
   }
 }
 
